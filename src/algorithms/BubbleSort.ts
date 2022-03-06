c42f53dff6c7e5ab52599233564f4dfa207bf2cf
nealarch01 <archivalneal@gmail.com>
2022-03-06 14:15:59 -0800

    added bubblesort

---
+++ src/algorithms/BubbleSort.ts
@@ -0,0 +1,18 @@
+import Swap from "../utils/Swap";
+
+async function BubbleSort(ar: HTMLCollection, run: boolean, delay_ms: number): Promise<void> {
+    let n: number = ar.length;
+    for (let i = n - 1; i >= 0; i--) {
+        for (let j = 1; j < n; j++) {
+            if (ar[j] < ar[j - 1]) {
+                await new Promise<void>((resolve) => {
+                    setTimeout(() => {
+                        Swap(ar, j, j - 1);
+                    }, delay_ms);
+                });
+            }
+        }
+    }
+}
+
+export default BubbleSort;
